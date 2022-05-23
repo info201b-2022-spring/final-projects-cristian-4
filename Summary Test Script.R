f715573c8a2314b31b9380481cf2275e0b4e398e
silpa-a <102693193+silpa-a@users.noreply.github.com>
2022-05-22 17:17:39 -0700

    final commit

--- Summary Test Script.R
+++ Summary Test Script.R
@@ -10,31 +10,31 @@ latino_df <- read.csv("/Users/silpaajjarapu/Documents/INFO201/final-projects-cri
 #create summary info list
 summary_info <-list()
 
-#White registration numbers
+#White citizen registration numbers
 summary_info$num_observations <- nrow(white_df)
 
 summary_info$num_features <- ncol(white_df)  
 
 total_white_registered <- white_df[1,4]
 
-#Black registration numbers
+#Black citizen registration numbers
 summary_info$num_observations <- nrow(black_df)
 
 summary_info$num_features <- ncol(black_df)
 
 total_black_registered <- black_df[1,4]
-  
-#Latino registration numbers
+
+#Latino citizen registration numbers
 summary_info$num_observations <- nrow(latino_df)
 
 summary_info$num_features <- ncol(latino_df)
 
 total_latino_registered <- latino_df[1,4]
   
-#absolute difference between black and white
+#absolute difference between black and white registered individuals
 
 black_white_registration_difference <- abs(total_white_registered - total_black_registered)
   
-#absolute difference between latino and white
+#absolute difference between latino and white registered individuals
 
 latino_white_registration_difference <- abs(total_white_registered - total_latino_registered)
