623283d5f824ddbf71eac6374543992d8afc2dbb
silpa-a <102693193+silpa-a@users.noreply.github.com>
2022-06-02 13:06:37 -0700

    silpa's commit

---
+++ app.R
@@ -0,0 +1,9 @@
+install.packages("shiny")
+install.packages("tidyverse")
+install.packages("dplyr")
+library(dplyr)
+library(tidyverse)
+
+white_df <- read.csv("/Users/silpaajjarapu/Documents/INFO201/final-projects-cristian-4/white_alone_formatted.csv")
+black_df <- read.csv("/Users/silpaajjarapu/Documents/INFO201/final-projects-cristian-4/black_alone_formatted.csv")
+latino_df <- read.csv("/Users/silpaajjarapu/Documents/INFO201/final-projects-cristian-4/latino_alone_formatted.csv")
