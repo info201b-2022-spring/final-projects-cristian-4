3a36130c710abfab138d6f6ef41a2029fd2c979b
silpa-a <102693193+silpa-a@users.noreply.github.com>
2022-06-05 21:29:56 -0700

    silpa's (hopefully) final commit

--- app.R
+++ app.R
@@ -47,15 +47,15 @@ analysis_page <- tabPanel(
       h5("Controls"),
       sliderInput(
         inputId = "population",
-        label = "Filter by Citizen Population Number:",
+        label = "Filter by max population number:",
         min = 0,
         max = 182000,
         value = 182000
       )
     ),
     mainPanel(
-      plotOutput(outputId = "scatter", click = "plot_click"),
-      tableOutput(outputId = "specifics")
+      plotOutput(outputId = "scatter"),
+      tableOutput(outputId = "data")
     ),
   )
 )
@@ -103,7 +103,17 @@ interactive_page <- tabPanel(
 #Summary Page Takeaways
 summary_page <- tabPanel(
   "Summary Takeaway",
-  titlePanel("Summary Takeaways for Interactive Page 1"),
+   titlePanel("Summary Takeaways for Interactive Page 1"),
+   p("Through my analysis of the number of individuals who reported voting vs. the total citizen population, I found that among the 
+races I analyzed, the white race group was a clear outlier. The white group represented all three of the outliers on the graph. When looking at the
+total citizen population, the white race group was higher than the other race groups in this category and also in the reported voted category. This
+tells us that the white race group has a both a higher citizen population and reported voting. With my interactivity feature, as I use the slider 
+to lower the population number, there is a trend among the black and latino groups. Both groups are similar on citizen population in relation to the 
+reported voted, and represent the points on the graph that are closer to the 0, meaning a lower reported voted and citizen population. Through my analysis,
+I have concluded that the citizen population and reported voted have a direct, positive relationship (the higher the citizen population, the higher the
+reported voted). Similarly, I have concluded that the white citizen population in relation to reported voted is greatly higher than that of the black
+and latino race groups."),
+  titlePanel("Summary Takeaways for Interactive Page 2"),
   p("When looking specifically at the breakdown of voting registration in all three races 
     based on age groups, it is evident those aged between 18 to 64 years old are the most consistent 
     in registering. This could be becasue these individuals are further educated about the current 
@@ -115,16 +125,6 @@ summary_page <- tabPanel(
     increase in the Black Lives Matter movement, there would be more 18 to 25 year olds registering to 
     vote as this age group is the backbone for majority of the political activisim everyone sees 
     today."),
-  titlePanel("Summary Takeaways for Interactive Page 2"),
-   p("Through my analysis of the number of individuals who reported voting vs. the total citizen population, I found that among the 
-races I analyzed, the white race group was a clear outlier. The white group represented all three of the outliers on the graph. When looking at the
-total citizen population, the white race group was higher than the other race groups in this category and also in the reported voted category. This
-tells us that the white race group has a both a higher citizen population and reported voting. With my interactivity feature, as I use the slider 
-to lower the population number, there is a trend among the black and latino groups. Both groups are similar on citizen population in relation to the 
-reported voted, and represent the points on the graph that are closer to the 0, meaning a lower reported voted and citizen population. Through my analysis,
-I have concluded that the citizen population and reported voted have a direct, positive relationship (the higher the citizen population, the higher the
-reported voted). Similarly, I have concluded that the white citizen population in relation to reported voted is greatly higher than that of the black
-and latino race groups."),
  titlePanel("Summary Takeaways for Interactive Page 3"),
   p("cristians stuff here")
 )
